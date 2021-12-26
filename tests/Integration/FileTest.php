e22bef61a6cc9d9fbc80713c47dc887439571a0c
Kenneth Trecy Tobias <web.kennethtobias@gmail.com>
2021-12-26 20:41:55 +0800

    Create integration tests for file

---
+++ tests/Integration/FileTest.php
@@ -0,0 +1,120 @@
+<?php
+
+namespace Tests\Integration;
+
+use Tests\TestCase;
+use Illuminate\Http\UploadedFile;
+use Illuminate\Support\Facades\Storage;
+use League\Flysystem\RootViolationException;
+use KennethTrecy\Virdafils\Util\GeneralHelper;
+use KennethTrecy\Virdafils\Node\File;
+
+class FileTest extends TestCase {
+	public function testRootImageCreation() {
+		$path = "/image";
+		$image = UploadedFile::fake()->image("a.png");
+
+		Storage::putFile($path, $image);
+
+		$this->assertDatabaseCount("files", 1);
+		$this->assertDatabaseHas("files", [
+			"name" => $image->hashName(),
+			"type" => "image/png",
+			"contents" => base64_encode($image->get())
+		]);
+	}
+
+	public function testRootImageCreationFromStream() {
+		$path = "/downloaded_image.png";
+		$image = fopen($this->faker->imageUrl(3, 3), "r");
+		$copied_stream = GeneralHelper::createMemoryStream();
+		stream_copy_to_stream($image, $copied_stream);
+		$image_contents = stream_get_contents($copied_stream);
+		fclose($copied_stream);
+
+		Storage::put($path, $image);
+
+		$this->assertDatabaseCount("files", 1);
+		$this->assertDatabaseHas("files", [
+			"name" => "downloaded_image.png",
+			"type" => "image/png",
+			"contents" => base64_encode($image_contents)
+		]);
+	}
+
+	public function testRootTextRetrieval() {
+		$path = "/present.txt";
+		$present_file = File::factory()->setPath($path)->create();
+
+		$contents = Storage::get($path);
+
+		$this->assertEquals($present_file->contents, $contents);
+		$this->assertDatabaseCount("files", 1);
+		$this->assertDatabaseHas("files", [
+			"name" => basename($path),
+			"type" => "text/plain",
+			"contents" => base64_encode($contents)
+		]);
+	}
+
+	public function testRootTextDeletion() {
+		$path = "/present.txt";
+		$present_file = File::factory()->setPath($path)->create();
+
+		$hasDeleted = Storage::delete($path);
+
+		$this->assertTrue($hasDeleted);
+		$this->assertDatabaseCount("files", 0);
+		$this->assertDeleted($present_file);
+	}
+
+	public function testRootTextAppend() {
+		$path = "/present.txt";
+		$present_file = File::factory()->setPath($path)->create();
+		$content_to_append = "hello world";
+
+		$hasUpdated = Storage::append($path, $content_to_append);
+
+		$this->assertTrue($hasUpdated);
+		$this->assertDatabaseCount("files", 1);
+		// TODO: Make the test platform-agnostic
+		$this->assertDatabaseHas("files", [
+			"name" => basename($path),
+			"type" => "text/plain",
+			"contents" => base64_encode("$present_file->contents\r\n$content_to_append")
+		]);
+	}
+
+	public function testRootTextPrepend() {
+		$path = "/present.txt";
+		$present_file = File::factory()->setPath($path)->create();
+		$content_to_prepend = "hello world";
+
+		$hasUpdated = Storage::prepend($path, $content_to_prepend);
+
+		$this->assertTrue($hasUpdated);
+		$this->assertDatabaseCount("files", 1);
+		// TODO: Make the test platform-agnostic
+		$this->assertDatabaseHas("files", [
+			"name" => basename($path),
+			"type" => "text/plain",
+			"contents" => base64_encode("$content_to_prepend\r\n$present_file->contents")
+		]);
+	}
+
+	public function testRootTextModification() {
+		$path = "/present.txt";
+		$present_file = File::factory()->setPath($path)->create();
+		$new_content = "hello world";
+
+		$hasUpdated = Storage::put($path, $new_content);
+
+		$this->assertTrue($hasUpdated);
+		$this->assertDatabaseCount("files", 1);
+		$this->assertDatabaseHas("files", [
+			"name" => basename($path),
+			"type" => "text/plain",
+			"contents" => base64_encode($new_content)
+		]);
+	}
+}
