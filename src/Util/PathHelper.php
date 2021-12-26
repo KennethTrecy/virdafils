57b73400dec151b7320ce1896ed726f2a9cae9f8
Kenneth Trecy Tobias <web.kennethtobias@gmail.com>
2021-12-26 17:51:13 +0800

    Create basic functions to join and split path

--- src/Util/PathHelper.php
+++ src/Util/PathHelper.php
@@ -5,4 +5,30 @@ namespace KennethTrecy\Virdafils\Util;
 class PathHelper {
 	const SEPARATOR = "/";
 	const ABSOLUTE_ROOT = "/";
+
+	/**
+	 * Joins directory names into one path (unresolved).
+	 *
+	 * Note: It will have root at the front of the string regardless whethere the root already exist
+	 * or not in the parameter. Empty directory names will be filtered out.
+	 *
+	 * @param array $names
+	 * @return string
+	 */
+	public static function join(array $names) {
+		$filtered_names = array_filter($names, function($name) {
+			return $name !== "" && $name !== static::SEPARATOR;
+		});
+		return static::SEPARATOR.implode(static::SEPARATOR, $filtered_names);
+	}
+
+	/**
+	 * Split directory into individual names but does not resolve it.
+	 *
+	 * @param string $directory_name
+	 * @return string
+	 */
+	public static function split(array $path) {
+		return explode(static::SEPARATOR, $path);
+	}
 }
