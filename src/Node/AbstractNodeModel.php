89a2688bcddd6e9ecb9d74fe001962de29ea1013
Kenneth Trecy Tobias <web.kennethtobias@gmail.com>
2021-12-26 18:07:32 +0800

    Create abstract node model

---
+++ src/Node/AbstractNodeModel.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace KennethTrecy\Virdafils\Node;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+abstract class AbstractNodeModel extends Model {
+	use HasFactory;
+}
