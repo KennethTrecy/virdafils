32bddb8ca1ff9b71a9ef5f7761d77063b7dacd6a
Kenneth Trecy Tobias <web.kennethtobias@gmail.com>
2022-02-09 21:17:04 +0800

    Remove update methods since they cannot be upgraded

--- src/VirdafilsAdapter.php
+++ src/VirdafilsAdapter.php
@@ -271,37 +271,6 @@ class VirdafilsAdapter implements FilesystemAdapter {
 			});
 	}
 
-	public function update($path, $contents, Config $configuration) {
-		$content_stream = GeneralHelper::createWrittenMemoryStream($contents);
-
-		$info = $this->updateStream($path, $content_stream, $configuration);
-		$info["contents"] = $contents;
-
-		return $info;
-	}
-
-	public function updateStream($path, $resource, Config $configuration) {
-		return $this->whenFileExists($path, function($file, $resolved_path) use ($resource) {
-			$contents = $file->contents;
-
-			if (is_string($contents)) {
-				$file->contents = stream_get_contents($resource);
-			} else {
-				$file->contents = $resource;
-			}
-
-			if ($file->save() && fclose($resource)) {
-				return [
-					"type" => "file",
-					"path" => $resolved_path,
-					"visibility" => $file->visibility
-				];
-			} else {
-				return false;
-			}
-		});
-	}
-
 	public function rename($old_path, $new_path) {
 		$path_parts = PathHelper::resolvedSplit($old_path, $this->configuration);
 
