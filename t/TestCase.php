a99f0f379061b636bbace71a93eef56f53d0f4eb
Kenneth Trecy Tobias <web.kennethtobias@gmail.com>
2022-02-09 11:52:39 +0800

    Rename the `tests` to `t` directory

--- t/TestCase.php
+++ t/TestCase.php
@@ -2,16 +2,31 @@
 
 namespace Tests;
 
-use Orchestra\Testbench\TestCase as BaseTestCase;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use KennethTrecy\Virdafils\VirdafilsServiceProvider;
 
-class TestCase extends BaseTestCase {
+class TestCase extends \Orchestra\Testbench\TestCase {
+	use RefreshDatabase;
+	use WithFaker;
+
+	protected $loadEnvironmentVariables = true;
+
 	protected function defineEnvironment($app) {
-
+		$app["config"]->set("filesystems.disks.virdafils", [
+			"driver" => "virdafils",
+			"root" => "/",
+			"visbility" => "private"
+		]);
 	}
 
 	protected function getPackageProviders($app) {
 		return [
-
+			VirdafilsServiceProvider::class
 		];
+	}
+
+	protected function defineDatabaseMigrations() {
+		$this->loadLaravelMigrations();
 	}
 }
