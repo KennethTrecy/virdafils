3e9acbe7db8da3fe94953d8ccce7bd57fa61d16f
Kenneth Trecy Tobias <web.kennethtobias@gmail.com>
2022-02-10 12:22:36 +0800

    Upgrade metadata tests

---
+++ t/Feature/MetadataTest.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace Tests\Feature;
+
+use Tests\TestCase;
+use KennethTrecy\Virdafils\VirdafilsAdapter;
+use KennethTrecy\Virdafils\Node\File;
+use KennethTrecy\Virdafils\Node\Directory;
+
+class MetadataTest extends TestCase {
+	public function testDeepFileMimeType() {
+		$adapter = new VirdafilsAdapter([]);
+		$path = "/a/sample.txt";
+		$file = File::factory()->setPath($path)->create();
+
+		$attributes = $adapter->mimeType($path);
+
+		$this->assertEquals($file->type, $attributes->mimeType());
+		$this->assertDatabaseCount("directories", 2);
+		$this->assertDatabaseCount("files", 1);
+	}
+
+	public function testRootFileSize() {
+		$adapter = new VirdafilsAdapter([]);
+		$path = "/present.txt";
+		$file = File::factory()->setPath($path)->create();
+
+		$attributes = $adapter->fileSize($path);
+
+		$this->assertEquals(strlen($file->contents), $attributes->fileSize());
+		$this->assertDatabaseCount("directories", 1);
+		$this->assertDatabaseCount("files", 1);
+	}
+
+	public function testRootFileLastModified() {
+		$adapter = new VirdafilsAdapter([]);
+		$path = "/present.txt";
+		$file = File::factory()->setPath($path)->create();
+
+		$attributes = $adapter->lastModified($path);
+
+		$this->assertEquals($file->updated_at->timestamp, $attributes->lastModified());
+		$this->assertDatabaseCount("directories", 1);
+		$this->assertDatabaseCount("files", 1);
+	}
+}
