79bcf924d4b7a9458cd07c74e13f8148b8641af5
Kenneth Trecy Tobias <web.kennethtobias@gmail.com>
2022-02-13 14:46:08 +0800

    Implement mechanism to access the file system adapter's test cases

--- t/Integration/FullTest.php
+++ t/Integration/FullTest.php
@@ -2,16 +2,55 @@
 
 namespace Tests\Integration;
 
+use Tests\TestCase;
 use League\Flysystem\Filesystem;
+use KennethTrecy\Virdafils\Directory;
 use League\Flysystem\FilesystemAdapter;
 use KennethTrecy\Virdafils\VirdafilsAdapter;
 use League\Flysystem\AdapterTestUtilities\FilesystemAdapterTestCase;
 
-class FullTest extends FilesystemAdapterTestCase
+class FullTest extends TestCase
 {
-    protected static function createFilesystemAdapter(): FilesystemAdapter
+    protected static FilesystemAdapterTestCase $tester;
+
+    public static function setUpBeforeClass(): void
     {
-        $configuration = [];
-        return new VirdafilsAdapter($configuration);
+        parent::setUpBeforeClass();
+        static::$tester = new class () extends FilesystemAdapterTestCase {
+            protected static function createFilesystemAdapter(): FilesystemAdapter
+            {
+                $configuration = [];
+                return new VirdafilsAdapter($configuration);
+            }
+
+            public static function setUpAll()
+            {
+                static::setUpBeforeClass();
+            }
+
+            public static function tearDownAll()
+            {
+                static::tearDownAfterClass();
+            }
+        };
+        get_class(static::$tester)::setUpBeforeClass();
+    }
+
+    public static function tearDownAfterClass(): void
+    {
+        parent::tearDownAfterClass();
+        get_class(static::$tester)::tearDownAfterClass();
+    }
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+        static::$tester->setUp();
+    }
+
+    protected function tearDown(): void
+    {
+        parent::tearDown();
+        static::$tester->tearDown();
     }
 }
